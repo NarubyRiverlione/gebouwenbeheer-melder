9e80a4ff72b5650950acef24ec73b600c518091a
Naruby Riverlione <naruby.riverlione@gmail.com>
2025-08-29 11:26:24 +0200

    strict type  rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,26 @@
 // eslint.config.mjs
+import eslint from "@eslint/js"
 import tseslint from "@typescript-eslint/eslint-plugin"
 import tsparser from "@typescript-eslint/parser"
 import prettierPlugin from "eslint-plugin-prettier"
-import prettierConfig from "eslint-config-prettier"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+import globals from "globals"
+import ts from "typescript"
 
 export default [
+  { ignores: ["dist", "node_modules", "**/*.config.*", "jest.config.js"] },
+
+  eslint.configs.recommended,
+ 
   {
     files: ["src/*.ts"],
 
-    languageOptions: {
-      parser: tsparser,
-      sourceType: "module",
-    },
+      languageOptions: {
+        parser: tsparser,
+        sourceType: "module",
+        parserOptions: { project: ["./tsconfig.json"] },
+        globals: globals.node,
+      },
 
     plugins: {
       "@typescript-eslint": tseslint,
@@ -20,10 +29,12 @@ export default [
 
     rules: {
       ...tseslint.configs.recommended.rules,
-      ...prettierConfig.rules,
+      ...tseslint.configs['flat/strict-type-checked'].rules,
+      ...tseslint.configs['flat/stylistic-type-checked'].rules,
       "@typescript-eslint/no-unused-vars": "warn",
       "no-console": "off",
       "prettier/prettier": "error",
     },
   },
+  eslintConfigPrettier,
 ]
